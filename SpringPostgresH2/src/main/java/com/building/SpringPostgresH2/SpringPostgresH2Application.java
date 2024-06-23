0c79c63a7210f27eff0c568edc7f9e37a88d814d
Aditya kumar <ar.adityakumar@gmail.com>
2024-06-23 16:53:30 +0530

    first commit

---
+++ SpringPostgresH2/src/main/java/com/building/SpringPostgresH2/SpringPostgresH2Application.java
@@ -0,0 +1,13 @@
+package com.building.SpringPostgresH2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPostgresH2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPostgresH2Application.class, args);
+	}
+
+}
